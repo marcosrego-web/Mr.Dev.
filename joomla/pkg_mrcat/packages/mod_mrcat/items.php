5b6e6a2dd0b81694e47c9937ab037f287ed05c7a
Marcos Rego <43761910+marcosrego-web@users.noreply.github.com>
2019-12-03 19:51:44 +0000

    Code ready for "Keyboard" pagination toggle

--- joomla/pkg_mrcat/packages/mod_mrcat/items.php
+++ joomla/pkg_mrcat/packages/mod_mrcat/items.php
@@ -259,16 +259,16 @@ defined('_JEXEC') or die;
 						}
 						$pagecount = ($pagecount - 1);
 						if($pagecount > 1) {
-							if( empty( $pagetoggles ) || !in_array( 1, $pagetoggles ) && !in_array( 1, $pagetoggles ) && !in_array( 2, $pagetoggles ) && !in_array( 3, $pagetoggles ) && !in_array( 4, $pagetoggles ) || in_array( 0, $pagetoggles )) {
-								echo  '<button class="mrwid-arrows mrwid-prev mrwid-'.$pagetransition.'" value="'.$pagecount.'"><span><</span></button>';
+							if( empty( $pagetoggles ) || !in_array( 1, $pagetoggles ) && !in_array( 1, $pagetoggles ) && !in_array( 2, $pagetoggles ) && !in_array( 3, $pagetoggles ) && !in_array( 4, $pagetoggles ) && !in_array( 5, $pagetoggles ) || in_array( 0, $pagetoggles )) {
+								echo  '<button class="mrwid-arrows mrwid-prev mrwid-'.$pagetransition.'"><span><</span></button>';
 							}
 							if( in_array( 3, $pagetoggles ) || in_array( 4, $pagetoggles )) {
 								echo  '<button class="'.((in_array(3, $pagetoggles))?'mrwid-below':"").' '.((in_array(4, $pagetoggles))?'mrwid-scroll':"").' mrwid-'.$pagetransition.'"><span>+</span></button>';
 							}
-							if( empty( $pagetoggles ) || !in_array( 1, $pagetoggles ) && !in_array( 1, $pagetoggles ) && !in_array( 2, $pagetoggles ) && !in_array( 3, $pagetoggles ) && !in_array( 4, $pagetoggles ) || in_array( 0, $pagetoggles )) {
-								echo  '<button class="mrwid-arrows mrwid-next mrwid-'.$pagetransition.'" value="2"><span>></span></button>';
+							if( empty( $pagetoggles ) || !in_array( 1, $pagetoggles ) && !in_array( 1, $pagetoggles ) && !in_array( 2, $pagetoggles ) && !in_array( 3, $pagetoggles ) && !in_array( 4, $pagetoggles ) && !in_array( 5, $pagetoggles ) || in_array( 0, $pagetoggles )) {
+								echo  '<button class="mrwid-arrows mrwid-next mrwid-'.$pagetransition.'"><span>></span></button>';
 							}
-							echo  '<div class="mrwid-pagination mrwid-'.$pagetransition.'">';
+							echo  '<div class="mrwid-pagination '.((in_array(5, $pagetoggles))?'mrwid-keyboard':"").' mrwid-'.$pagetransition.'">';
 								$hideelement = '';
 								if( empty( $pagetoggles ) || !in_array( 1, $pagetoggles )) {
 									$hideelement = 'style="display:none;"';
